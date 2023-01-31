27c9c4b29d43082c1941a65af1deddb5b2d7f14a
Lucas Cristovam <cristhovamlucas@gmail.com>
2023-01-31 18:06:46 -0300

    Next steps...

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import './styles/global.css';
-
-import Habit from './components/Habit';
+// import Habit from './components/Habit';
+import { Plus } from 'phosphor-react';
+import logoImage from './assets/logo.svg';
+import Header from './components/Header';
 
 function App() {
      return (
-          <>
-               <Habit completed={3} />
-               <Habit />
-               <Habit />
-               <Habit />
-               <Habit />
-          </>
+          <div className='w-screen h-screen flex justify-center items-center'>
+               <div className='w-full max-w-5xl px-6 flex-col gap-16'>
+                    <Header />
+               </div>
+          </div>
      );
 };
 
